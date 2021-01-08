23406c87989f951ecf6f562580bbcd1cb01a6e0e
entirelymagic <entirelymagic@gmail.com>
2021-01-08 15:24:49 +0200

    Added profiles viewset and set permissions to modify only its own profile

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -7,6 +7,7 @@ from profiles_api import views
 
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, basename='hello-viewset')  # no need for slash
+router.register('profile', views.UserProfileViewSet)  # no need to provide base set - because of the queryset provided
 
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
