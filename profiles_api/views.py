f9e57ccba1080818b7a6fc134589d7b6ef7c0ad9
entirelymagic <entirelymagic@gmail.com>
2021-01-08 12:05:51 +0200

    Added POST, PUT, PATCH and DELETE method to  our APIView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,9 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializers
 
 
 class HelloApiView(APIView):
     """Test API View"""
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """Returns a list of APIView features"""
@@ -15,3 +19,32 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """Create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)  # standard use of serializer class in a serializer view
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')  # retrieve the name from the serializer object
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def put(self, request, pk=None):
+        """Handle updating an entire object"""
+        return Response({'method': 'PUT'})
+
+    def patch(self, request, pk=None):
+        """Handle a partial update of an object"""
+        # just providing a last name instead of replacing the entire fields of the object as an example
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        """Delete an object in the database"""
+        return Response({'method': 'DELETE'})
+
+
